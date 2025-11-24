cf91911eedb781296565f151a6ba569b005f3aca
Namrata Dalvi <namratadalvi642@gmail.com>
2025-11-24 19:15:47 +0530

    Update login API endpoint to production URL

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
       formPayload.append("username", formData.email); // backend expects username field
       formPayload.append("password", formData.password);
 
-      const res = await fetch("http://127.0.0.1:8000/auth/login", {
+      const res = await fetch("https://prescriptionreader-4x86.onrender.com//auth/login", {
         method: "POST",
         headers: { 
           "Content-Type": "application/x-www-form-urlencoded"
